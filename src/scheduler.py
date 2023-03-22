b865903dc3483cd0ccd27a836d7bd37ba6575832
Zachary Muller <Zachlmuller@ou.edu>
2023-03-22 14:48:53 -0500

    updated scheduler to have a better interface, added better logos, cleaned up code

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,16 +1,16 @@
 import streamlit as st
+from st_aggrid import AgGrid
 import pandas as pd
 
-
 def main():
-    st.sidebar.image("./images/index.png")
-    st.sidebar.image("./images/outransparent.png")
+    st.sidebar.image("./images/msci.png")
+    st.sidebar.image("./images/gcoeou.png")
     st.title("Liquid Thunders Payment Scheduler")
 
 
     payments = st.sidebar.selectbox("Select the payment schedule", ["Weekly", "Bi-Weekly", "Monthly", "Bi-Monthly", "Quarterly", "Semi-Annually", "Annually"])
     start_date = st.date_input("Start Date")
-    end_date = st.date_input("End Date")
+    end_date = st.date_input("End Date", min_value=start_date, value=start_date)
 
     # Computer the number of payments and dates of payments based on the payment schedule
     if payments == "Weekly":
@@ -35,15 +35,24 @@ def main():
         num_payments = end_date.year - start_date.year
         payment_dates = [start_date + pd.DateOffset(years=i) for i in range(num_payments)]
 
+    # convert payment_dates to dataframe with month, day, and year columns
+    payment_dates = pd.DataFrame({"Month": [date.strftime('%B') for date in payment_dates],
+                                    "Day": [date.day for date in payment_dates],
+                                    "Year": [date.year for date in payment_dates]})
+
     # Display the payment schedule with the payment dates and date range
     if num_payments > 0:
         st.write("Payment Schedule: ", payments)
         st.write("Number of Payments: ", num_payments)
         st.write("Date Range: ", start_date, " to ", end_date)
-        st.table(pd.DataFrame({"Payment Date": payment_dates}))
+        AgGrid(payment_dates, fit_columns_on_grid_load=True)
     else:
         st.write("No payments in the date range") 
 
+    # Button to download the dataframe as a csv file
+    if st.button("Download Payment Schedule"):
+        payment_dates.to_csv("payment_schedule.csv", index=False)
+        st.write("Downloaded payment schedule as csv file")
 
 if __name__ == "__main__":
     main()
